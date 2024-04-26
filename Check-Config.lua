4691aa297b0ff81cb31046cabc1462f0908a7caa
Paul Emmerich <tandanu@deadlybossmods.com>
2024-04-26 20:17:50 +0200

    Enable global-element check and add mode to only check globals
    This disallows implicitly declared globals.
    
    CLI flag `--only_check_globals` will only validate globals to be a quick
    and lightweight replacement for LuaCheck

--- Check-Config.lua
+++ Check-Config.lua
@@ -1,5 +1,6 @@
 local fs   = require "bee.filesystem"
 local json = require "json"
+local diags = require "proto.diagnostic"
 
 if not DBM_LIBRARIES then
 	error("Usage: parameter --dbm_libraries is required")
@@ -69,18 +70,41 @@ if not found then
 end
 trustedPluginsFile:close()
 
+local disabledDiagnostics = {
+	"unused-local", -- Very spammy and low value
+	"redefined-local", -- Low value
+	"empty-block", -- Triggers on some locales, generally not very useful
+	"invisible", -- Slowest diagnostic and we don't use it
+	"deprecated", -- Second slowest diagnostic and also pretty much unused for us
+	"duplicate-doc-field" -- Slow to run on mods, likely due to the auto-generated field annotations (which we don't need to check)
+}
+
+-- Lightweight check just to replace LuaCheck but without pulling in more noisy/potentially false-positive warnings.
+-- Used to gate releases.
+if ONLY_CHECK_GLOBALS then
+	local enabledDiagnostics = {
+		["undefined-global"] = true,
+		["lowercase-global"] = true,
+		["global-element"] = true
+	}
+	for diag in pairs(diags.getDiagAndErrNameMap()) do
+		if not enabledDiagnostics[diag] then
+			disabledDiagnostics[#disabledDiagnostics + 1] = diag
+		end
+	end
+	-- Config file is run before the plugin that loads these, so they are not yet included above
+	disabledDiagnostics[#disabledDiagnostics + 1] = "dbm-sync-checker"
+	disabledDiagnostics[#disabledDiagnostics + 1] = "dbm-event-checker"
+end
+
 return {
 	["Lua.workspace.library"] = libs,
 	["Lua.runtime.version"] = "Lua 5.1",
 	["Lua.runtime.plugin"] = pluginPath,
 	["Lua.diagnostics.globals"] = globals,
-	["Lua.diagnostics.disable"] = {
-		"unused-local", -- Very spammy and low value
-		"redefined-local", -- Low value
-		"empty-block", -- Triggers on some locales, generally not very useful
-		"invisible", -- Slowest diagnostic and we don't use it
-		"deprecated", -- Second slowest diagnostic and also pretty much unused for us
-		"duplicate-doc-field" -- Slow to run on mods, likely due to the auto-generated field annotations (which we don't need to check)
+	["Lua.diagnostics.disable"] = disabledDiagnostics,
+	["Lua.diagnostics.neededFileStatus"] = {
+		["global-element"] = "Any" -- Bans defining global variables, even if they are all uppercase (consistent with LuaCheck)
 	},
 	["Lua.diagnostics.severity"]  = {
 		["undefined-global"] = "Error",
