b6916bebf5863b2aa15b19a6651ee4f52dc14fa8
Paul Emmerich <tandanu@deadlybossmods.com>
2025-01-06 11:43:18 +0100

    Fix LibDropDownMenu definitions

--- Definitions/Dependencies.lua
+++ Definitions/Dependencies.lua
@@ -18,10 +18,10 @@ function LibDeflate:DecompressDeflate(str) end
 
 ---@class LibDropDownMenu
 LibDropDownMenu = {}
-function LibDropDownMenu:UIDropDownMenu_SetWidth(frame, newWidth) end
-function LibDropDownMenu:UIDropDownMenu_AddButton(info, level) end
-function LibDropDownMenu:UIDropDownMenu_Initialize(frame, initFunction, displayMode, level, menuList) end
-function LibDropDownMenu:ToggleDropDownMenu(level, value, dropDownFrame, anchorName, xOffset, yOffset, menuList, button, autoHideDelay, overrideDisplayMode) end
+function LibDropDownMenu.UIDropDownMenu_SetWidth(frame, newWidth) end
+function LibDropDownMenu.UIDropDownMenu_AddButton(info, level) end
+function LibDropDownMenu.UIDropDownMenu_Initialize(frame, initFunction, displayMode, level, menuList) end
+function LibDropDownMenu.ToggleDropDownMenu(level, value, dropDownFrame, anchorName, xOffset, yOffset, menuList, button, autoHideDelay, overrideDisplayMode) end
 ---@class DropdownMenu
 ---@return DropdownMenu
-function LibDropDownMenu:Create_DropDownMenu(name, parent) end
+function LibDropDownMenu.Create_DropDownMenu(name, parent) end
